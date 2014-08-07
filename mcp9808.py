e9fa5f4e12ee264a10ba8cd594c908ba7cf845ca
Stuart <stuart.feichtinger@gmail.com>
2014-08-06 19:22:50 -0500

    Read temperature working

--- mcp9808.py
+++ mcp9808.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
 import sys
 import Adafruit_I2C as ada_i2c
 from registers import MCP9808_Register
@@ -23,11 +25,9 @@ class MCP9808:
 
   def begin(self):
     self.reset()
-    self.__manID = self.__i2c.readU16(self.register.MANUF_ID)
-    self.__deviceID = self.__i2c.readU16(self.register.DEVICE_ID)
-    print(hex(self.__manID))
-    print(hex(self.__deviceID))
-    if(self.__manID != 0x5400):
+    self.__manID = self.readU16(self.register.MANUF_ID)
+    self.__deviceID = self.readU16(self.register.DEVICE_ID)
+    if(self.__manID != 0x0054):
       raise RuntimeError("Wrong Manufacturer ID: %s. (Should be 0x5400. Check I2C Address)" %  hex(self.__manID))
 
     if(self.__deviceID != 0x4):
@@ -36,14 +36,15 @@ class MCP9808:
 
 
   def reset(self):
-    self.__i2c.write16(self.register.CONFIG, 0x0000)
-    self.__i2c.write16(self.register.UPPER_TEMP, 0x0000)
-    self.__i2c.write16(self.register.LOWER_TEMP, 0x0000)
-    self.__i2c.write16(self.register.CRIT_TEMP, 0x0000)
+    self.write16(self.register.CONFIG, 0x0000)
+    self.write16(self.register.UPPER_TEMP, 0x0000)
+    self.write16(self.register.LOWER_TEMP, 0x0000)
+    self.write16(self.register.CRIT_TEMP, 0x0000)
     self.__i2c.write8(self.register.RESOLUTION, 0x03)
 
-  def readTemp(self, unit = 'C'):
-    rawData = self.__i2c.readU16(self.register.AMBIENT_TEMP)
+  def getTemp(self, unit = 'C'):
+
+    rawData = self.readU16(self.register.AMBIENT_TEMP)
     if(rawData & (1<< 16)):
       self.criticalFlag = 1
 
@@ -53,15 +54,37 @@ class MCP9808:
     if(rawData & (1 << 14)):
       self.lowerFlag = 1
 
+    temp = rawData & 0x0FFF
+
+    uByte = (temp >> 8)
+    lByte = (temp & 0x00FF)
+
+    temp = ((temp & 0x0F00 ) >> 4) + ((temp & 0x00FF) / 16)
+
+    if(rawData & 0x1000):
+      temp = 256 - temp
+
+    if(unit == 'F'):
+      temp /= 5
+      temp *= 9
+      temp += 32
+    return temp
+
 
   def shutdown(self):
     self.__i2c.writeU16(self.register.CONFIG, self.register.SHUTDOWN)
 
 
+  def readU16(self, register):
+    return self.__i2c.reverseByteOrder(self.__i2c.readU16(register))
+
+  def write16(self, register, data):
+    self.__i2c.write16(register, self.__i2c.reverseByteOrder(data))
 
 def main():
   therm = MCP9808()
   therm.begin()
+  print("The temperature is currently {} °C.".format(therm.getTemp()))
 
   return 0
 
